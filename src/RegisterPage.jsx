1f04fd17a2fba3febbedbcf823f53b21f763f965
Zachary <zbergman2701@gmail.com>
2025-11-23 23:04:37 -0500

    Fixed Register Page

--- src/RegisterPage.jsx
+++ src/RegisterPage.jsx
@@ -1,3 +1,31 @@
+import { Link } from "react-router";
+
 export function RegisterPage() {
-  return <div>Hello Register</div>;
+  return (
+    <div>
+      <h1>Register for an account</h1>
+      <div>
+        <div>First Name</div>
+        <input></input>
+      </div>
+      <div>
+        <div>Last Name</div>
+        <input></input>
+      </div>
+      <div>
+        <div>Email</div>
+        <input></input>
+      </div>
+      <div>
+        <div>Password</div>
+        <input></input>
+      </div>
+      <div className="register">
+        <button>Register</button>
+      </div>
+      <div>
+        <Link to="/login">Already have an account? Log in here</Link>
+      </div>
+    </div>
+  );
 }
